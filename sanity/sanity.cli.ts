45e69a4ff8483b1947f7c87407045b190e9f9b1d
HTM <haythamaaraba7@gmail.com>
2023-08-28 21:19:07 +0100

    backend setup

---
+++ sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'gfrxz5cd',
+    dataset: 'production'
+  }
+})
---
+++ sanity/schemas/index.ts
@@ -0,0 +1,10 @@
+import testimonials from './testimonials'
+import abouts from './abouts'
+import contact from './contact'
+import brands from './brands'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import works from './works'
+
+export const schemaTypes = [ works, testimonials, brands, abouts, skills, workExperience, experiences, contact ]
