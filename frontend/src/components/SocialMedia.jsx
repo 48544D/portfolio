a6d6311e6677f2e4c339504b9b1d3a0dc7408999
HTM <haythamaaraba7@gmail.com>
2023-09-06 22:41:05 +0100

    Socials update

--- frontend/src/components/SocialMedia.jsx
+++ frontend/src/components/SocialMedia.jsx
@@ -1,18 +1,23 @@
 import React from "react";
-import { BsTwitter, BsInstagram } from "react-icons/bs";
-import { FaFacebookF } from "react-icons/fa";
+import { BsGithub, BsLinkedin } from "react-icons/bs";
+// import { FaFacebookF } from "react-icons/fa";
 
 const SocialMedia = () => {
+  const linkToGit = () => {
+    window.open("https://github.com/48544D");
+  };
+
+  const linkToLinkedin = () => {
+    window.open("https://www.linkedin.com/in/aaraba-haytham-1684a1233/");
+  };
+
   return (
     <div className="app__social">
-      <div>
-        <BsTwitter />
+      <div onClick={linkToGit}>
+        <BsGithub />
       </div>
-      <div>
-        <FaFacebookF />
-      </div>
-      <div>
-        <BsInstagram />
+      <div onClick={linkToLinkedin}>
+        <BsLinkedin />
       </div>
     </div>
   );
